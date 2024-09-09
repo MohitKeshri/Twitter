6c9fa838d6055749ed9035162c47205d2258e7e8
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-09 13:03:31 +0530

    Added codes

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connect = require("./config/database");
+
+const app = express();
+
+const TweetRepository = require("./repository/tweet-repository");
+const Comment = require("./models/comments");
+
+app.listen(3000, async () => {
+  console.log(`server started`);
+  await connect();
+  console.log("Mongodb connected");
+  // const tweet = await Tweet.create({
+  //   content: "third tweet",
+  //   userEmail: "a@b.com",
+  // });
+  const tweetRepo = new TweetRepository();
+  //console.log(tweet);
+
+  // console.log(tweet);
+  // tweet.comments.push({ content: "first comment" });
+  // await tweet.save();
+  // console.log(tweet);
+});
