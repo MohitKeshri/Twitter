502dbb2695ff61a3fce737cfd8b666bf3700f4c2
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-10 00:34:32 +0530

    started adding create service for tweets

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -29,15 +29,6 @@ class TweetRepository {
     }
   }
 
-  async update(id, data) {
-    try {
-      const tweet = await Tweet.findByIdAndUpdate(id, data, { new: true });
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   async destroy(id) {
     try {
       await Tweet.findByIdAndDelete(id);
