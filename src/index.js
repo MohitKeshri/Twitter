bbc5e3ae10acd82ac38cb2c001c5de636a0530fa
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-10 22:22:05 +0530

    added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ const connect = require("./config/database");
 
 const app = express();
 
-const TweetRepository = require("./repository/tweet-repository");
-const Comment = require("./models/comments");
-
 app.listen(3000, async () => {
   console.log(`server started`);
   await connect();
