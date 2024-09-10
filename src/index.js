d39d8135b468be78aaddbaf4d59532074bb96bee
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-11 00:39:47 +0530

    added index to hashtag and removed hashtag array from the tweet schema

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { connect } from "./config/database.js";
 
 const app = express();
 
-// import Service from "./service/tweet-service.js";
+import Service from "./service/tweet-service.js";
 
 app.listen(3000, async () => {
   console.log(`server started`);
   await connect();
   console.log("Mongodb connected");
-  // let ser = new Service();
-  // await ser.create({ content: "Done with #refactor ?" });
+  let ser = new Service();
+  await ser.create({
+    content: "other #CoDE #works or #NOT with no #hashTAG #array ?",
+  });
 });
