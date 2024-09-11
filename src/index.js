31c66f36ae7a6ab56abbbcaadba4c861b2913260
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-11 23:43:57 +0530

    Toggle likes working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import { connect } from "./config/database.js";
 
 import apiRoutes from "./routes/index.js";
+import { UserRepository, TweetRepository } from "./repository/index.js";
+import LikeService from "./service/like-service.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +16,12 @@ app.listen(3000, async () => {
   console.log(`server started`);
   await connect();
   console.log("Mongodb connected");
+
+  const userrepo = new UserRepository();
+  const tweetrepo = new TweetRepository();
+  const tweets = await tweetrepo.getAll(0, 10);
+  const users = await userrepo.getAll();
+
+  const likeService = new LikeService();
+  await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
