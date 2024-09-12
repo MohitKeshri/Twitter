08402b8e486a100ebb1d5f1ce669366adf637db6
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-12 22:16:02 +0530

    added signup with encrypted password

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import bodyParser from "body-parser";
 import { connect } from "./config/database.js";
 
 import apiRoutes from "./routes/index.js";
-import { UserRepository, TweetRepository } from "./repository/index.js";
-import LikeService from "./service/like-service.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,12 +14,4 @@ app.listen(3000, async () => {
   console.log(`server started`);
   await connect();
   console.log("Mongodb connected");
-
-  // const userrepo = new UserRepository();
-  // const tweetrepo = new TweetRepository();
-  // const tweets = await tweetrepo.getAll(0, 10);
-  // const users = await userrepo.getAll();
-
-  // const likeService = new LikeService();
-  // await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,12 +3,17 @@ import express from "express";
 import { createTweet, getTweet } from "../../controllers/tweet-controller.js";
 import { toggleLike } from "../../controllers/like-controller.js";
 import { createComment } from "../../controllers/comment-controller.js";
+import { signup } from "../../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.post("/tweets", createTweet);
 router.get("/tweets/:id", getTweet);
+
 router.post("/likes/toggle", toggleLike);
+
+router.post("/signup", signup);
+
 router.post("/comments", createComment);
 
 export default router;
