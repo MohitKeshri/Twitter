239a37fb616313e9376aedcb275e0d7897568e36
Mohit Keshri <sendtomohit99@gmail.com>
2024-09-20 21:02:08 +0530

    Added basic setup for jest

---
+++ src/service/dummy-service.js
@@ -0,0 +1,7 @@
+import { helper } from "./helper-service";
+export const execute = () => {
+  const result = helper();
+  if (result) {
+    return "learning js";
+  } else return "learning reactjs";
+};
